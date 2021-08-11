7506beaf55d617ab92d3f6712e8f9b666f7cdd95
astizm <61395917+astizm@users.noreply.github.com>
2021-08-11 15:20:39 +1200

    rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,19 @@
+"""random dice game"""
+#main function
 def main():
-   print("You've rolled a die")
+  #import random package
+  import random
+  #set roll limit
+  dice_rolls = 2
+  dice_sum = 0
+  for i in range(0, dice_rolls):
+   #set random outcome to variable with range 1-6
+   roll = random.randint(1,6)
+   dice_sum += roll
+   #print outcome
+   print(f'You rolled a {roll}')
+   print(f'You have rolled a total of {dice_sum}')
+
 
 if __name__ == "__main__":
   main()
