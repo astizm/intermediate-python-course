b5ace2c2ed5bfa30990b651d4e90b2065bc5c24f
astizm <61395917+astizm@users.noreply.github.com>
2021-08-11 15:31:21 +1200

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -3,16 +3,24 @@
 def main():
   #import random package
   import random
-  #set roll limit
-  dice_rolls = 2
+  #set roll limit and dice details
+  dice_rolls = int(input('How many dice would you like to roll?'))  
   dice_sum = 0
+  dice_size = int(input('How many sides are the dice?'))
   for i in range(0, dice_rolls):
-   #set random outcome to variable with range 1-6
-   roll = random.randint(1,6)
+   #set random outcome to variable with dice size range
+   roll = random.randint(1,dice_size)
    dice_sum += roll
    #print outcome
-   print(f'You rolled a {roll}')
-   print(f'You have rolled a total of {dice_sum}')
+   if roll == 1:
+    print(f'You rolled a {roll}! Critical Fail')
+    quit()
+   elif roll == dice_size:
+     print(f'You rolled a {roll}! Critical Success')     
+   else:
+    print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {dice_sum}')
+
 
 
 if __name__ == "__main__":
