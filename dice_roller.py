b8f7adb70f3e07c192390c52b23f650954c180a2
astizm <asti-maera@live.nmit.ac.nz>
2021-08-24 13:55:27 +1200

    synch

--- dice_roller.py
+++ dice_roller.py
@@ -3,24 +3,36 @@
 def main():
   #import random package
   import random
+  
+  #set player detail
+  player = str(input("What's your name? "))
   #set roll limit and dice details
-  dice_rolls = int(input('How many dice would you like to roll?'))  
+  roll = int
+  dice_rolls = int(input('How many dice would you like to roll? '))  
   dice_sum = 0
-  dice_size = int(input('How many sides are the dice?'))
+  dice_size = int(input('How many sides are the dice? '))
+  
   for i in range(0, dice_rolls):
    #set random outcome to variable with dice size range
    roll = random.randint(1,dice_size)
    dice_sum += roll
-   #print outcome
+   #add score to list
+   scores = list((player,roll, dice_sum))
+   #print outcome 
    if roll == 1:
     print(f'You rolled a {roll}! Critical Fail')
+    print(scores)
     quit()
    elif roll == dice_size:
      print(f'You rolled a {roll}! Critical Success')     
    else:
     print(f'You rolled a {roll}')
     print(f'You have rolled a total of {dice_sum}')
+  
 
+    
+  print(scores)
+  
 
 
 if __name__ == "__main__":
